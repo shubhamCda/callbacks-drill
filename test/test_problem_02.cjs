bd3d28cbba12d650ff4271579ef0eb087b5dd887
Shubham <shubham@gmail.com>
2024-09-26 14:32:46 +0530

    Add delete_files function

--- test/test_problem_02.cjs
+++ test/test_problem_02.cjs
@@ -1,5 +1,32 @@
-// import read_write_delete from "../problem_02.cjs";
+const path = require("path");
 
-const read_write_delete = require("../problem_02.cjs");
 
-console.log(read_write_delete());
+const input_file_path = path.join(__dirname, "lipsum.txt");
+const uppercase_file_path = path.join(__dirname, "upperCase.txt");
+const lowercase_file_path = path.join(__dirname, "lowerCase.txt");
+const sorted_file_path = path.join(__dirname, "sorted.txt");
+const filenames_path = path.join(__dirname, "filenames.txt");
+
+
+
+const { read_source_files, convert_content_uppercase, append_filenames, convert_content_lowercase, sort_content, delete_files } = require("../problem_02.cjs");
+
+
+
+
+read_source_files(input_file_path, (data) =>{
+    convert_content_uppercase(data, uppercase_file_path, (filePath) =>{
+        append_filenames(filenames_path, filePath, () =>{
+            convert_content_lowercase(uppercase_file_path, lowercase_file_path, (filePath) =>{
+                append_filenames(filenames_path, filePath, () =>{
+                    sort_content(sorted_file_path, lowercase_file_path, (filePath) =>{
+                        append_filenames(filenames_path, filePath, () =>{
+                            delete_files(filenames_path);
+                        })
+                    })
+                });
+            });
+        });
+    });
+    
+});
