147611a3dd77f11b2390bfa4b394e6908378e42b
Shubham <shubham@gmail.com>
2024-09-26 14:53:07 +0530

    fix indentation in test_problem_02

--- test/test_problem_02.cjs
+++ test/test_problem_02.cjs
@@ -14,13 +14,13 @@ const { read_source_files, convert_content_uppercase, append_filenames, convert_
 
 
 
-read_source_files(input_file_path, (data) =>{
-    convert_content_uppercase(data, uppercase_file_path, (filePath) =>{
-        append_filenames(filenames_path, filePath, () =>{
-            convert_content_lowercase(uppercase_file_path, lowercase_file_path, (filePath) =>{
-                append_filenames(filenames_path, filePath, () =>{
-                    sort_content(sorted_file_path, lowercase_file_path, (filePath) =>{
-                        append_filenames(filenames_path, filePath, () =>{
+read_source_files(input_file_path, (data) => {
+    convert_content_uppercase(data, uppercase_file_path, (filePath) => {
+        append_filenames(filenames_path, filePath, () => {
+            convert_content_lowercase(uppercase_file_path, lowercase_file_path, (filePath) => {
+                append_filenames(filenames_path, filePath, () => {
+                    sort_content(sorted_file_path, lowercase_file_path, (filePath) => {
+                        append_filenames(filenames_path, filePath, () => {
                             delete_files(filenames_path);
                         })
                     })
@@ -28,5 +28,5 @@ read_source_files(input_file_path, (data) =>{
             });
         });
     });
-    
+
 });
