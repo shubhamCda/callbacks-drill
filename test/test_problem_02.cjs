ba9c2948ecad7273af9f0a7efc58f5b567505253
Shubham <shubham@gmail.com>
2024-10-02 10:05:26 +0530

    Add main function

--- test/test_problem_02.cjs
+++ test/test_problem_02.cjs
@@ -1,32 +1,11 @@
-const path = require("path");
+const { problem_02_callbacks } = require("../problem_02.cjs");
 
 
-const input_file_path = path.join(__dirname, "lipsum.txt");
-const uppercase_file_path = path.join(__dirname, "upperCase.txt");
-const lowercase_file_path = path.join(__dirname, "lowerCase.txt");
-const sorted_file_path = path.join(__dirname, "sorted.txt");
-const filenames_path = path.join(__dirname, "filenames.txt");
+function main() {
+    problem_02_callbacks();
+}
 
-
-
-const { file_reader, convert_content_uppercase, append_filenames, convert_content_lowercase, sort_content, delete_files } = require("../problem_02.cjs");
+main();
 
 
 
-
-file_reader(input_file_path, (data) => {
-    convert_content_uppercase(data, uppercase_file_path, (filePath) => {
-        append_filenames(filenames_path, filePath, () => {
-            convert_content_lowercase(uppercase_file_path, lowercase_file_path, (filePath) => {
-                append_filenames(filenames_path, filePath, () => {
-                    sort_content(sorted_file_path, lowercase_file_path, (filePath) => {
-                        append_filenames(filenames_path, filePath, () => {
-                            delete_files(filenames_path);
-                        })
-                    })
-                });
-            });
-        });
-    });
-
-});
